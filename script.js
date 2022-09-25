e73f8a357ef345d4d4cedf473eba25c4bbb0307d
inzhefop <76850445+inzhefop@users.noreply.github.com>
2022-09-25 13:01:50 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function btn00() {
+    $(function(){
+        $("#page_content").load("sub_pages/main_page.html"); 
+      });
+}
+
+function btn01() {
+    $(function(){
+        $("#page_content").load("sub_pages/extra_utilities_reborn/eur.html"); 
+      });
+}
